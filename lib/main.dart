ac31885c83d2f5917bc82a4193d65d5dbc14f7c6
oiji-official <xyz.notimportant@gmail.com>
2021-03-21 22:57:24 +0530

    initial root folder name

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:memory_monarch/utils/config/theme.dart';
 import 'package:memory_monarch/views/home/homePage.dart';
 
+//ROOT FOLDER NAME IS memory_monarch
+//change file paths accordingly or rename the root folder to aforementioned name.
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
